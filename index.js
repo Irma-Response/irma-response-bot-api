2526afd1f74817f9f5c6c3301a18213589e0d0f2
Tom Dooner <tdooner@codeforamerica.org>
2017-09-07 13:09:11 -0700

    Fix request and response format for API.AI
    It is very particular about the response format -- it says "Response is
    empty" if it cannot parse the response.
    
    Also, the `Content-Type: application/json` needed to be set.

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const zipCodes = require('./lib/zipCodes.js');
 const apiClient = require('./lib/apiClient.js');
 
 exports.sheltersByZip = function sheltersByZip(req, res) {
-  const zip = req.body.zip || req.query.zip;
+  // sample post body from API.AI:
+  // { id: '585f4b0f-01b7-463b-929b-c2fdaac3a92d', timestamp:
+  // '2017-09-07T19:42:26.559Z', lang: 'en', result: { source: 'agent',
+  // resolvedQuery: 'where is the nearest shelter?', speech: '', action:
+  // 'lookup-shelter', actionIncomplete: true, parameters: { zip: '' },
+  // contexts: [ [Object], [Object], [Object] ], metadata: { intentId:
+  // '676cf61c-278f-47bc-919e-72b7e19470f4', webhookUsed: 'true',
+  // webhookForSlotFillingUsed: 'true', intentName: 'where is the nearest
+  // shelter' }, fulfillment: { speech: 'What zip code?', messages: [Object] },
+  // score: 1 }, status: { code: 200, errorType: 'success' }, sessionId:
+  // '28bc69a3-e6ca-4ab7-b2a7-c5277e2e57b1' }
+  const zip = req.query.zip || (req.body.result &&
+    req.body.result.parameters.zip);
+
+  console.log('returning results for zip', zip);
 
   if (zip === undefined || zipCodes === undefined) {
     res.status(500).send('No zip given, or zip code geolocation data unavilable!');
@@ -14,6 +28,7 @@ exports.sheltersByZip = function sheltersByZip(req, res) {
     apiClient.sheltersByLatLon(lat, lon).then(data => {
       // TODO: Format this response in a way that makes sense for API.AI
       // (happening in apiClient right now, but broken)
+      res.set('Content-Type', 'application/json');
       res.send(data);
       res.status(200).end();
     }).catch(e => {
