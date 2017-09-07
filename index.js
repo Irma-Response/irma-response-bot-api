9c02b775126835f9dc95747db16f049093558dea
tabinfl <tabinfl@gmail.com>
2017-09-07 13:33:13 -0400

    Added JSON wrapper to response for api.ai

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const zipCodes = require('./lib/zipCodes.js');
 const apiClient = require('./lib/apiClient.js');
 
 exports.sheltersByZip = function helloWorld(req, res) {
-  if (req.body.zip === undefined && req.query.zip === undefined) {
-    res.status(404).send('No zip given!');
+  zip = req.body.result.parameters['zip'] || req.query[zip];
+  if (zip === undefined || zipCodes === undefined) {
+    res.status(500).send('No zip given, or zip code geolocation data unavilable!');
   } else {
-    const [lat, lon] = zipCodes[req.body.zip || req.query.zip];
+    const [lat, lon] = zipCodes[zip];
 
     apiClient.sheltersByLatLon(lat, lon).then(data => {
-      // TODO: Format this response in a way that makes sense for API.AI
+      // TODO: Format this response in a way that makes sense for API.AI (happening in apiClient right now, but broken)
       res.send(data);
       res.status(200).end();
     }).catch(e => {
