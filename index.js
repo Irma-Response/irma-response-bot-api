e2e5a91838642185362ddb17e749d113b3d17b59
Tom Dooner <tdooner@codeforamerica.org>
2017-09-07 12:33:33 -0700

    Add 404 error if no ZIP code found

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const zipCodes = require('./lib/zipCodes.js');
 const apiClient = require('./lib/apiClient.js');
 
-exports.sheltersByZip = function helloWorld(req, res) {
+exports.sheltersByZip = function sheltersByZip(req, res) {
   const zip = req.body.zip || req.query.zip;
 
   if (zip === undefined || zipCodes === undefined) {
     res.status(500).send('No zip given, or zip code geolocation data unavilable!');
+  } else if (!(zip in zipCodes)) {
+    res.status(404).send('Could not find ZIP code');
   } else {
     const [lat, lon] = zipCodes[zip];
 
     apiClient.sheltersByLatLon(lat, lon).then(data => {
-      // TODO: Format this response in a way that makes sense for API.AI (happening in apiClient right now, but broken)
+      // TODO: Format this response in a way that makes sense for API.AI
+      // (happening in apiClient right now, but broken)
       res.send(data);
       res.status(200).end();
     }).catch(e => {
