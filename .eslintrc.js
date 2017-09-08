7cd23aff6032a4e0b33826759ef4d2d0cfdf926b
Tom Dooner <tdooner@codeforamerica.org>
2017-09-08 15:50:05 -0700

    Extract response formatting from apiClient
    This is a pretty sizeable shift but the conditional logic was getting
    too dicy in the apiClient.
    
    I separated the responsibility of generating the API.AI response into
    its own module that accepts a shelter as a parameter.
    
    I also anticipate a lot more logic in parsing API.AI webhooks, so I
    separated that into its own file as well.
    
    This adds eslint as well, which helps improve code quality and catch
    simple errors.

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+    "comma-dangle": 0
+  }
+};
--- index.js
+++ index.js
@@ -1,32 +1,15 @@
-const zipCodes = require('./lib/zipCodes.js');
+const apiAiResponseFormatter = require('./lib/apiAiResponseFormatter');
 const apiClient = require('./lib/apiClient.js');
-
-Params = {
-    zip : undefined,
-    lang : "en"
-};
-
+const parseApiAiBody = require('./lib/parseApiAiBody');
+const zipCodes = require('./lib/zipCodes.js');
 
 exports.sheltersByZip = function sheltersByZip(req, res) {
-  // sample post body from API.AI:
-  // { id: '585f4b0f-01b7-463b-929b-c2fdaac3a92d', timestamp:
-  // '2017-09-07T19:42:26.559Z', lang: 'en', result: { source: 'agent',
-  // resolvedQuery: 'where is the nearest shelter?', speech: '', action:
-  // 'lookup-shelter', actionIncomplete: true, parameters: { zip: '' },
-  // contexts: [ [Object], [Object], [Object] ], metadata: { intentId:
-  // '676cf61c-278f-47bc-919e-72b7e19470f4', webhookUsed: 'true',
-  // webhookForSlotFillingUsed: 'true', intentName: 'where is the nearest
-  // shelter' }, fulfillment: { speech: 'What zip code?', messages: [Object] },
-  // score: 1 }, status: { code: 200, errorType: 'success' }, sessionId:
-  // '28bc69a3-e6ca-4ab7-b2a7-c5277e2e57b1' }
-  
-  const zip = req.query.zip || (req.body.result &&
-    req.body.result.parameters.zip);
-  Params.zip = zip;
-  
-  Params.lang = req.body.result.parameters.lang || "en";
+  const parsedRequest = parseApiAiBody(req.body);
+  const zip = req.query.zip || parsedRequest.zip;
+  const lang = parsedRequest.lang || 'en';
 
-  console.log('returning results for zip', Params.zip, ', language: ', Params.lang);
+  // eslint-disable-next-line
+  console.log('returning results for zip', zip, ', language: ', lang);
 
   if (zip === undefined || zipCodes === undefined) {
     res.status(500).send('No zip given, or zip code geolocation data unavilable!');
@@ -35,14 +18,13 @@ exports.sheltersByZip = function sheltersByZip(req, res) {
   } else {
     const [lat, lon] = zipCodes[zip];
 
-    apiClient.sheltersByLatLon(lat, lon, Params).then(data => {
-      // TODO: response packaging for api.ai belongs here (currently in apiClient.js)
+    apiClient.sheltersByLatLon(lat, lon).then(({ shelter }) => {
       res.set('Content-Type', 'application/json');
-      res.send(data);
+      res.send(JSON.stringify(apiAiResponseFormatter.formatShelterMessage(shelter, lang)));
       res.status(200).end();
-    }).catch(e => {
+    }).catch((e) => {
       res.send(e.message);
       res.status(500).end();
-    });;
+    });
   }
 };
